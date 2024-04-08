427df36c48eeb8dea009921f4e8def79958606b0
adrielze <oadriel14@gmail.com>
2024-04-08 08:55:25 -0300

    Leitura e manipulação de elementos XML

---
+++ app_curso_XML/PrettyPrint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace app_curso_XML
+{
+    namespace MySite.Common.BizTalkUtil
+    {
+        public static class XmlFormatter
+        {
+            // Neal Walters - added 09/11/2015 from 
+            // http://stackoverflow.com/questions/203528/what-is-the-simplest-way-to-get-indented-xml-with-line-breaks-from-xmldocument
+            public static string PrettyPrint(this XmlDocument doc)
+            {
+                var stringWriter = new StringWriter(new StringBuilder());
+                var xmlTextWriter = new XmlTextWriter(stringWriter) { Formatting = Formatting.Indented };
+                doc.Save(xmlTextWriter);
+                return stringWriter.ToString();
+            }
+        }
+    }
+}
---
+++ app_curso_XML/Program.cs
@@ -0,0 +1,180 @@
+﻿using app_curso_XML.MySite.Common.BizTalkUtil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace XMLDocument_Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlInputFilename = @"C:\Users\adrie\Desktop\AULAS XML 2024\IntroSamples\Flight03.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlInputFilename);
+
+            Console.WriteLine("\n\n File= " + xmlInputFilename);
+            Console.WriteLine(xmlDoc.OuterXml);
+
+          
+          
+
+            string formattedXML = XmlFormatter.PrettyPrint(xmlDoc);
+
+            Console.WriteLine(xmlDoc.OuterXml);
+            Console.WriteLine("\n\n");
+            Console.WriteLine(formattedXML);
+
+            Console.WriteLine("\n\n File= " + xmlInputFilename);
+           
+
+            //How to select a single node:
+            Console.WriteLine("\n\n");
+            string xpath = "/AirlineName";
+
+            XmlNode node = xmlDoc.SelectSingleNode(xpath);
+
+            Console.WriteLine("Node Value: " + GetNodeValue(node));
+
+            //How to select multiple nodes:
+            string xpathMultiple = "//DepartureAirport";
+            XmlNodeList xmlNodeList = xmlDoc.SelectNodes(xpathMultiple);
+
+            Console.WriteLine("Number of Matching Nodes = " + xmlNodeList.Count);
+            Console.WriteLine("Querie for Xpath: " + xpathMultiple);
+
+            foreach(XmlNode nodeX in xmlNodeList)
+            {
+                Console.WriteLine("Node = " + GetNodeValue(nodeX));
+            }
+
+
+            //How to select an element by Tag Name:
+            XmlNodeList nodeList2 = xmlDoc.GetElementsByTagName("DepartureAirport");
+            //This is the same as using xPath with the // at the beginning of the node name.
+
+
+            //How to navigate between node.
+            //Navigation Methods: parentNode, childNodes, firstChild, lastChild, nextSibling, previousSibling
+            string xpathNav = "//DepartureAirport";
+         
+            XmlNode nodeNav1 = xmlDoc.SelectSingleNode(xpathNav);
+
+            Console.WriteLine("nodeNav1= " + GetNodeValue(nodeNav1) + " for xPath " + xpathNav);
+              
+            XmlNode nextSiblingNode = nodeNav1.NextSibling;
+            Console.WriteLine("nextSiblingNode.Name = " + nextSiblingNode.Name + " Value = " +
+                GetNodeValue(nextSiblingNode));
+
+
+            XmlNode prevSiblingNode = nodeNav1.PreviousSibling;
+            Console.WriteLine("prevSiblingSiblingNode.Name = " + prevSiblingNode.Name + " Value = " +
+               GetNodeValue(prevSiblingNode));
+
+            XmlNode parentNode = nodeNav1.ParentNode;
+            Console.WriteLine("parentNode.Name = " + parentNode.Name + " Value = " + GetNodeValue(parentNode));
+
+            if (parentNode.NodeType.ToString() == "Element")
+            {
+                XmlElement parentElement = (XmlElement)parentNode;
+                Console.WriteLine("FlightLeg/@seq= " + parentElement.GetAttribute("seq"));
+            }
+            else
+            {
+                Console.WriteLine("Expecting parent of DepartureAirport to ben an element but it was not.");
+            }
+
+            //How to validate XML data against and XSD Schema
+            string schemaFilename = @"C:\Users\adrie\Desktop\AULAS XML 2024\IntroSamples\Flight03_dotNet.xsd";
+            string validXMLFilename = @"C:\Users\adrie\Desktop\AULAS XML 2024\IntroSamples\Flight03Min.xml";
+            string validationResult = ValidateXMLFileAgainstSchema(validXMLFilename, schemaFilename);
+            Console.WriteLine("First Validation Result: " + validationResult);
+
+
+            string invalidXMLFilename = @"C:\Users\adrie\Desktop\AULAS XML 2024\IntroSamples\Flight03_Bad.xml";
+            validationResult = ValidateXMLFileAgainstSchema(invalidXMLFilename,schemaFilename);
+            Console.WriteLine("result: " + validationResult);
+
+            //How to add an atribute called 'Direct' with value "yes" or "no" to the first Flight
+            Console.WriteLine("Adding new 'direct' atribute to first Flight");
+            string xpathFlight1 = "//Flight";
+            XmlNodeList nodeListFlight1 = xmlDoc.SelectNodes(xpathFlight1);
+
+            foreach(XmlNode flightNode in nodeListFlight1)
+            {
+                string varYesNo = "yes";
+
+                string xPathFlightLegForCount = "FlightLeg";
+                XmlNodeList nodeListFlightLegs = flightNode.SelectNodes(xPathFlightLegForCount);
+                if (nodeListFlightLegs.Count > 1)
+                {
+                    varYesNo = "no";
+                }
+
+                ((XmlElement)flightNode).SetAttribute("direct", varYesNo);
+            }
+
+            //string formattedXML3 = XmlFormatter.PrettyPrint(xmlDoc);
+            //Console.WriteLine(formattedXML3);
+
+
+            //How to add an element called 'SeatAssignment' with value 'temp'
+            Console.WriteLine("Adding new 'SeatAssignment' atribute");
+            string xpathFlightLeg1 = "//FlightLeg";
+            XmlNodeList nodeListFlightLeg1 = xmlDoc.SelectNodes(xpathFlightLeg1);
+
+            foreach (XmlNode flightLegNode in nodeListFlightLeg1)
+            {
+                XmlElement newSeatElement = xmlDoc.CreateElement("SeatAssignment");
+                newSeatElement.InnerText = "temp";
+                flightLegNode.AppendChild(newSeatElement);
+            }
+
+            string formattedXML3 = XmlFormatter.PrettyPrint(xmlDoc);
+            Console.WriteLine(formattedXML3);
+
+
+
+
+
+        }
+        static string GetNodeValue(XmlNode nodeArg)
+        {
+            if (nodeArg ==null)
+            {
+                return "";
+            }
+            else
+            {
+                return nodeArg.FirstChild.Value;
+            }
+        }
+
+        static string ValidateXMLFileAgainstSchema(string xmlPath, string xsdPath)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(xmlPath);
+
+            xml.Schemas.Add(null, xsdPath);
+
+            try
+            {
+                xml.Validate(null);
+                return "Success";
+            }
+            catch (XmlSchemaValidationException ex)
+            {
+                Console.WriteLine("Second validation: ValidationError: ");
+                return ex.Message;
+            }
+        }
+    }
+}
+
+    
